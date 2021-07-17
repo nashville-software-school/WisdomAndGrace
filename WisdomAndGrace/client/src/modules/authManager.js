dd7ead6c6764b3ae3e903af850e35b899fc9a678
Andy Collins <askingalot@gmail.com>
2021-07-17 18:10:48 -0500

    fix a timing bug in authManager

--- WisdomAndGrace/client/src/modules/authManager.js
+++ WisdomAndGrace/client/src/modules/authManager.js
@@ -1,45 +1,29 @@
 import firebase from "firebase/app";
 import "firebase/auth";
 
-const _apiUrl = "/api/userprofile";
 
-const _doesUserExist = (firebaseUserId) => {
-  return getToken().then((token) =>
-    fetch(`${_apiUrl}/DoesUserExist/${firebaseUserId}`, {
-      method: "GET",
-      headers: {
-        Authorization: `Bearer ${token}`
-      }
-    }).then(resp => resp.ok));
+export const getToken = () => {
+  const currentUser = firebase.auth().currentUser;
+  if (!currentUser) {
+    throw new Error("Cannot get current user. Did you forget to login?");
+  }
+  return currentUser.getIdToken();
 };
-
-const _saveUser = (userProfile) => {
-  return getToken().then((token) =>
-    fetch(_apiUrl, {
-      method: "POST",
-      headers: {
-        Authorization: `Bearer ${token}`,
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(userProfile)
-    }).then(resp => resp.json()));
-};
-
-
-
-export const getToken = () => firebase.auth().currentUser.getIdToken();
 
 
 export const login = (email, pw) => {
   return firebase.auth().signInWithEmailAndPassword(email, pw)
-    .then((signInResponse) => _doesUserExist(signInResponse.user.uid))
+    .then((signInResponse) => {
+      loginStatus.apply(AUTH_ACTIONS.FIREBASE_LOGIN);
+      return _doesUserExist(signInResponse.user.uid);
+    })
     .then((doesUserExist) => {
-      if (!doesUserExist) {
-
-        // If we couldn't find the user in our app's database, we should logout of firebase
-        logout();
-
-        throw new Error("Something's wrong. The user exists in firebase, but not in the application database.");
+      if (doesUserExist) {
+        loginStatus.apply(AUTH_ACTIONS.API_LOGIN);
+      } else {
+        return logout().then(() => {
+          throw new Error("Something's wrong. The user exists in firebase, but not in the application database.");
+        });
       }
     }).catch(err => {
       console.error(err);
@@ -49,21 +33,110 @@ export const login = (email, pw) => {
 
 
 export const logout = () => {
-  firebase.auth().signOut()
+  return firebase.auth().signOut()
+    .then(() => loginStatus.apply(AUTH_ACTIONS.LOGOUT));
 };
 
 
 export const register = (userProfile, password) => {
   return firebase.auth().createUserWithEmailAndPassword(userProfile.email, password)
-    .then((createResponse) => _saveUser({ 
-      ...userProfile, 
-      firebaseUserId: createResponse.user.uid 
-    }));
+    .then((createResponse) => {
+      loginStatus.apply(AUTH_ACTIONS.FIREBASE_LOGIN);
+      return _saveUser({ ...userProfile, firebaseUserId: createResponse.user.uid })
+    })
+    .then(() => loginStatus.apply(AUTH_ACTIONS.API_LOGIN));
 };
 
 
-export const onLoginStatusChange = (onLoginStatusChangeHandler) => {
+export function onLoginStatusChange(handler) {
+  loginStatus.setOnLoginStatusChangedHandler(handler);
+
   firebase.auth().onAuthStateChanged((user) => {
-    onLoginStatusChangeHandler(!!user);
+    if (user) {
+      loginStatus.apply(AUTH_ACTIONS.REINSTATE_FIREBASE_LOGIN);
+    } else {
+      loginStatus.apply(AUTH_ACTIONS.LOGOUT);
+    }
   });
 };
+
+
+const _apiUrl = "/api/userprofile";
+
+function _doesUserExist(firebaseUserId) {
+  return getToken().then((token) =>
+    fetch(`${_apiUrl}/DoesUserExist/${firebaseUserId}`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    }).then(resp => resp.ok));
+}
+
+function _saveUser(userProfile) {
+  return getToken().then((token) =>
+    fetch(_apiUrl, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(userProfile)
+    }).then(resp => resp.json()));
+}
+
+const loginStatus = {
+  _state: INITIAL,
+  get _isLoggedIn() {
+    return this._state === INITIAL ? null : this._state === AUTHENTICATED;
+  },
+  apply(action) {
+    const prevIsLoggedIn = this._isLoggedIn;
+    this._state = this._state(action);
+    this._loginStatusChanged(prevIsLoggedIn);
+  },
+  setOnLoginStatusChangedHandler(handler) {
+    handler(this._isLoggedIn);
+    this._onLoginStatusChangedHandler = handler;
+  },
+  _onLoginStatusChangedHandler() { },
+  _loginStatusChanged(prevIsLoggedIn) {
+    if (prevIsLoggedIn !== this._isLoggedIn) {
+      this._onLoginStatusChangedHandler(this._isLoggedIn);
+    }
+  }
+};
+
+const AUTH_ACTIONS = ['REINSTATE_FIREBASE_LOGIN', 'FIREBASE_LOGIN', 'API_LOGIN', 'LOGOUT']
+  .reduce((actions, action) => ({ ...actions, [action]: action }), {});
+
+function INITIAL(action) {
+  switch (action) {
+    case AUTH_ACTIONS.REINSTATE_FIREBASE_LOGIN: return AUTHENTICATED;
+    case AUTH_ACTIONS.FIREBASE_LOGIN: return FIREBASE_AUTHENTICATED_API_UNAUTHENTICATED;
+    case AUTH_ACTIONS.LOGOUT: return UNAUTHENTICATED;
+    default: return INITIAL;
+  }
+}
+
+function UNAUTHENTICATED(action) {
+  switch (action) {
+    case AUTH_ACTIONS.FIREBASE_LOGIN: return FIREBASE_AUTHENTICATED_API_UNAUTHENTICATED;
+    default: return UNAUTHENTICATED;
+  }
+}
+
+function FIREBASE_AUTHENTICATED_API_UNAUTHENTICATED(action) {
+  switch (action) {
+    case AUTH_ACTIONS.API_LOGIN: return AUTHENTICATED;
+    case AUTH_ACTIONS.LOGOUT: return UNAUTHENTICATED;
+    default: return FIREBASE_AUTHENTICATED_API_UNAUTHENTICATED;
+  }
+}
+
+function AUTHENTICATED(action) {
+  switch (action) {
+    case AUTH_ACTIONS.LOGOUT: return UNAUTHENTICATED;
+    default: return AUTHENTICATED;
+  }
+}
